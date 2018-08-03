477bb51730226a659d3f9f70af645e58758db520
Anish Ali <ali.anish91@yahoo.com>
2018-08-03 19:14:42 -0400

    added 3 more end points for finding all food trucks, finding food trucks by id, updating the info of the food truck and setup the deleting the food truck by id

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -5,6 +5,7 @@ import Foodtruck from '../model/foodtruck';
 export default({ config, db }) => {
   let api = Router();
 
+
   // '/v1/foodtruck/add'
   api.post('/add', (req, res) => {
     let newFoodT = new Foodtruck();
@@ -18,5 +19,51 @@ export default({ config, db }) => {
     });
   });
 
+  // '/v1/foodtruck/'
+  api.get('/', (req,res) => {
+    //find all foodtruck in db
+    Foodtruck.find({}, (err, foodtrucks) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(foodtrucks);
+    });
+  });
+
+  // 'v1/foodtruck/:id'
+  api.get('/:id', (req,res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(foodtruck);
+    });
+  });
+
+  // 'v1/foodtruck/:id'
+  api.put('/:id', (req,res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if(err) {
+        res.send(err);
+      }
+      foodtruck.name = req.body.name;
+      foodtruck.save(err => {
+        if(err) {
+          res.send(err);
+        }
+        res.json({message: "Foodtruck info updated"});
+      });
+    });
+  });
+
+  // 'v1/foodtruck/:id'
+  api.delete('/:id', (req, res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if(err) {
+        res.send(err);
+      }
+      
+    })
+  })
   return api;
 }
